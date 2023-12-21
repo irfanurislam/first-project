9bd5922db3a5d8e090041e152bbcb0e3b150e2a7
irfanurislam <mdirfanurislam@gmail.com>
2023-12-21 10:34:15 +0600

    handle validation mongoose eror

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,16 +1,48 @@
 // const app: Application = express();
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
 import cors from "cors";
-export const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSources } from "../interface/error";
+import config from "../config";
+import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next
 ) => {
-  const statusCode = err.statusCode ||500;
-  const message = err.message || "something went wrong";
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "something went wrong";
+
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "something went wrong",
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
+    stack: config.NODE_ENV === "development" ? err?.stack : null,
   });
 };
